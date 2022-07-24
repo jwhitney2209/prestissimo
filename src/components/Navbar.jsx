30d5e266580e6fbc15b3e8c9c04d5f30803aedb4
teachergg <jwhitney@bisd.us>
2022-07-24 15:41:18 -0500

    links on home and signup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 const Navbar = () => {
@@ -12,11 +13,10 @@ const Navbar = () => {
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black '>
       <h1 className='w-full text-3xl font-bold text-[#6A0DAD]'>PRESTISSIMO.</h1>
       <ul className='hidden md:flex items-center'>
-        <li className='p-4 font-medium hover:text-[#6A0DAD]'>Home</li>
+        <Link to="/" className='p-4 font-medium hover:text-[#6A0DAD]'>Home</Link>
         <li className='p-4 font-medium hover:text-[#6A0DAD]'>Pricing</li>
         <li className='p-4 font-medium hover:text-[#6A0DAD]'>About</li>
-        <button className="bg-[#6A0DAD] hover:bg-[#550a8a] py-2 w-[100px] rounded font-medium mx-2 text-white">Sign Up</button>
-        <button className="bg-gray-400 hover:bg-gray-500 py-2 w-[100px] rounded font-medium text-white">Log In</button>
+        <Link to="/signup" className="bg-[#6A0DAD] hover:bg-[#550a8a] flex justify-center py-2 w-[100px] rounded font-medium mx-2 text-white">Sign Up</Link>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}

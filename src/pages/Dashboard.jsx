63d779ef0fd0ff54b72ecbf7a6af5348b747a70b
teachergg <jwhitney@bisd.us>
2022-07-26 00:06:32 -0500

    dashboard placeholder text

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='max-w-[800px] w-full mx-auto text-center flex flex-col justify-center'>
+      <h1 className='md:text-4xl sm:text-3xl text-3xl font-bold md:py-6'>
+        Dashboard coming soon!
+      </h1>
+    </div>
   )
 }
 
